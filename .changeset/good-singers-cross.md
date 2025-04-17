4498c0d2e82314cc2cae661eb7672dc828be7853
samuelreichor <samuelreichor@gmail.com>
2025-04-17 18:48:50 +0200

    Update all npm packages and add support for admin() query

---
+++ .changeset/good-singers-cross.md
@@ -0,0 +1,6 @@
+---
+'vue-craftcms': patch
+---
+
+Update js-craftcms-api to latest version to get admin() user query.
+Update all npm packages
--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,7 +1,15 @@
 <script setup lang="ts">
-  import { useCraft } from '@vue-craftcms';
+  import { useCraft, useCraftUrlBuilder } from '@vue-craftcms';
   const { baseUrl, siteMap } = useCraft();
   console.log(baseUrl, siteMap);
+
+  const queryEntryUrl = useCraftUrlBuilder('entries')
+    .section('news')
+    .fields(['title'])
+    .limit(3)
+    .buildUrl('all');
+
+  const queryUserUrl = useCraftUrlBuilder('users').admin().buildUrl('all');
 </script>
 
 <template>
@@ -10,6 +18,8 @@
       <RouterLink to="/">Go to Home</RouterLink>
       <RouterLink to="/news-article-1">Go to News Article 1</RouterLink>
     </nav>
+    <p>Entry Url{{ queryEntryUrl }}</p>
+    <p>User Url{{ queryUserUrl }}</p>
     <Suspense>
       <RouterView />
     </Suspense>
